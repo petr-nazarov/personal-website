7e5e1f1b4c8f2102acd2f876b03937a62fee25f8
Nazarov Petr <mrzueck@gmail.com>
2020-01-10 12:42:31 +0200

    added bulma support

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nazarov-petr
-      </h1>
-      <h2 class="subtitle">
-        my personal website
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <logo />
   </div>
 </template>
 
@@ -33,36 +13,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
