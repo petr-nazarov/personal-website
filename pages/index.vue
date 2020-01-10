f19ba995171ba1bcf52abeec64086fee4dcdddc8
Nazarov Petr <mrzueck@gmail.com>
2020-01-10 12:02:40 +0200

    lint fix

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,7 @@
         my personal website
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
